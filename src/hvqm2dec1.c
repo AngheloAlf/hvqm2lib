102c026ba5e52da972b3d83766e28412a3e96037
angie <angheloalf95@gmail.com>
2023-03-21 00:25:02 -0300

    hvqm2Init1

--- src/hvqm2dec1.c
+++ src/hvqm2dec1.c
@@ -24,5 +24,35 @@ INCLUDE_ASM("asm/functions/hvqm2dec1", hvqm2Decode1);
 
 INCLUDE_ASM("asm/functions/hvqm2dec1", hvqm2Setup1);
 
-INCLUDE_ASM("asm/functions/hvqm2dec1", hvqm2Init1);
+#define B_009AC0_LEN 0x300
 
+extern s32 B_0092C0[0x200];
+extern u8 B_009AC0[B_009AC0_LEN];
+extern u8 B_009DC0[B_009AC0_LEN];
+
+void hvqm2Init1(u8 alpha) {
+    s32 i;
+    s32 var_a2;
+
+    alpha >>= 7;
+
+    for (i = 0, var_a2 = -0x100; i < B_009AC0_LEN; i++, var_a2++) {
+        if (var_a2 >= 0) {
+            if (var_a2 < 0x100) {
+                B_009AC0[i] = (var_a2 >> 2) & 0x3E;
+            } else {
+                B_009AC0[i] = 0x3E;
+            }
+        } else {
+            B_009AC0[i] = 0;
+        }
+
+        B_009AC0[i] = B_009AC0[i];
+        B_009DC0[i] = B_009AC0[i] | alpha;
+    }
+
+    B_0092C0[0] = 0;
+    for (i = 1; i < ARRAY_COUNT(B_0092C0); i++) {
+        B_0092C0[i] = 0x1000 / i;
+    }
+}
